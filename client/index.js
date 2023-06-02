071f34a20738c9b6594affc23491901d7228570c
Phillip Choi <38228979+Minsu386@users.noreply.github.com>
2023-06-02 14:06:17 -0400

    basic sass style completed

--- client/index.js
+++ client/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-
+import './index.scss'
 
 ReactDOM.render(
   <Provider store={store}>
     <div>Hello, world!</div>,
   
   </Provider>,
-  document.getElementById('app') // make sure this is the same as the id of the div in your index.html
+    document.getElementById('app') // make sure this is the same as the id of the div in your index.html
 );
