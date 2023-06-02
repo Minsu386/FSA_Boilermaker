05c0b5682d9d3aa644a537c2bc364f97933bb25c
Phillip Choi <38228979+Minsu386@users.noreply.github.com>
2023-06-02 19:33:03 -0400

    main component still inop

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,5 +1,7 @@
 const db = require('../db')
 const Sequelize = require('sequelize')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User =db.define('user', {
   username: {
@@ -17,6 +19,37 @@ const User =db.define('user', {
     }
   }
 })
+
+User.prototype.generateToken = function(){
+  return jwt.sign({ id: this.id}, process.env.JWT);
+}
+
+User.findByToken = async function(token){
+  const { id } = jwt.verify(token, process.env.JWT);
+  const user = await User.findByPk(id);
+  if(user){
+    return user;
+  }
+  throw 'nope';
+}
+
+User.authenticate = async function({ username, password }){
+  const user = await User.findOne({
+    where: {
+      username
+    }
+  });
+  if(user && await bcrypt.compare(password, user.password)){
+    return user.generateToken(); 
+  }
+
+  throw 'bad credentials';
+}
+
+User.addHook('beforeSave', async(user)=> {
+  user.password = await bcrypt.hash(user.password, 5);
+  console.log(user.password);
+});
 
 
 module.exports = User;
